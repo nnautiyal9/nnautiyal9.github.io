d729e0bd37c6451e17bcf6bb8d2d311fcb8c9637
nnautiyal9 <39855957+nnautiyal9@users.noreply.github.com>
2019-04-06 10:13:53 +0530

    Update smooth.js

--- smooth.js
+++ smooth.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -14,20 +12,3 @@ $(function() {
     }
   });
 });
-=======
-
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
->>>>>>> f0d86c36d16439b6fc044bec605dd4064bed5d30
